c3f0fa915b5904abe80d6794fff753c13a90b9fa
zachSelindh <zachselindh@gmail.com>
2022-09-19 13:42:39 -0500

    +'d pages and id route

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ import styles from '../styles/Home.module.css';
 import { Footer } from '../components/layout';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Never Not Writing</title>
-        <meta name="description" content="A notes app alternative because the elephant one is acting weird" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Footer />
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Never Not Writing</title>
+                <meta name="description" content="A notes app alternative because the elephant one is acting weird" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Footer />
+        </div>
+    );
 }
---
+++ pages/notes/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const NotesDetailPage = () => {
+    const router = useRouter();
+
+    return (
+        <div>
+            <h1>Notes detail page</h1>
+        </div>
+    );
+};
+
+export default NotesDetailPage;
