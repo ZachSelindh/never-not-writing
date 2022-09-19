dad58403354125a04afdd07016c6a12072ec4e08
zachSelindh <zachselindh@gmail.com>
2022-09-19 12:36:55 -0500

    TS config init

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,21 +1,22 @@
 import React from "react"
 import Image from 'next/image'
 
-import '../../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 const Footer = () => {
 
     return (
-        <footer className="footer">
+        <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}, created by a dude who needed his own notes app
-          <span className="logo">
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} color="white" />
           </span>
+          {' '} , created by a dude who needed his own notes app
         </a>
       </footer>
     )
